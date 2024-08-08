1383d4f4530d9fb40abe4feaac35a0baab59e3be
AlexRamosGlz <joralexfx@gmail.com>
2024-08-08 09:40:20 -0600

    user-api creation

--- src/router/Users.Router.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router } from "express";
-
-export const usersRouter: express.IRouter = Router();
-
-usersRouter.get("/", (req, res) => {
-    res.status(200).json({ message: 'router created'})
-})
